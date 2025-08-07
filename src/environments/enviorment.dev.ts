c3901d1a00dd6dea52f6259b731c85569f1aea21
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2025-08-06 22:12:19 -0500

    Added new pipeline

---
+++ src/environments/enviorment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  API_URL: 'http://contables.unicauca.edu.co:443/dev/api/',
+  myStorageUrl: 'https://api.cloudinary.com/v1_1/dz5pw4p7y/image/upload',
+  storageName: 'dz5pw4p7y',
+  storageDirectory: 'cloudinary-enterprise',
+  keycloak_url: 'http://contables.unicauca.edu.co:443/dev/api/keycloak/token/',
+  URL: 'http://contables.unicauca.edu.co:443/dev/',
+
+  //Cambiar la URL y el microservicio que se va a consumir en modo local
+  API_LOCAL_URL: 'http://localhost:8080/api/',
+  microservice: '' // accountCatalogue, product, enterprise
+};
