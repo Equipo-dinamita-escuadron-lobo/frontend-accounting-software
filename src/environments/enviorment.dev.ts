fd9da3bcd3f435db162231d5e004bd62f164b20d
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2025-08-07 21:11:14 -0500

    Updated environmnet url

--- src/environments/enviorment.dev.ts
+++ src/environments/enviorment.dev.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  API_URL: 'http://contables.unicauca.edu.co:443/dev/api/',
+  API_URL: 'http://contables.unicauca.edu.co/dev/api/',
   myStorageUrl: 'https://api.cloudinary.com/v1_1/dz5pw4p7y/image/upload',
   storageName: 'dz5pw4p7y',
   storageDirectory: 'cloudinary-enterprise',
-  keycloak_url: 'http://contables.unicauca.edu.co:443/dev/api/keycloak/token/',
-  URL: 'http://contables.unicauca.edu.co:443/dev/',
+  keycloak_url: 'http://contables.unicauca.edu.co/dev/api/keycloak/token/',
+  URL: 'http://contables.unicauca.edu.co/dev/',
 
   //Cambiar la URL y el microservicio que se va a consumir en modo local
   API_LOCAL_URL: 'http://localhost:8080/api/',
